179bd53776497c53fe94aa98a3863a367f59c971
kenti <016mki69@gmail.com>
2024-10-09 01:43:11 -0300

    back: feat(property): add property req and res dtos

--- backend/rentify/src/main/java/com/nocountry/rentify/dto/response/property/PropertyRes.java
+++ backend/rentify/src/main/java/com/nocountry/rentify/dto/response/property/PropertyRes.java
@@ -1,5 +1,6 @@
 package com.nocountry.rentify.dto.response.property;
 
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,4 +8,21 @@ import lombok.Data;
 @AllArgsConstructor
 public class PropertyRes {
 
+  private Long id;
+  private Long ownerId;
+  private String country;
+  private String province;
+  private String city;
+  private String streetName;
+  private String streetNumber;
+  private int rooms;
+  //private Antiquity antiquity;
+  private int yearsOfAntiquity;
+  private BigDecimal price;
+  private BigDecimal maintanceFees;
+  private String title;
+  private String description;
+  private BigDecimal totalArea;
+  private BigDecimal builtArea;
+  //private Status status;
 }
