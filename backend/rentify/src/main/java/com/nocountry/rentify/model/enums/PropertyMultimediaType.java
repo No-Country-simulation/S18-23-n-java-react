d167dce747c9a85ae3feca1ee72391121bc50e2c
kenti <016mki69@gmail.com>
2024-10-17 20:58:59 -0300

    back: feat(PropertyMultimediaType): make PropertyMultimediaType implement TranslatableEnum interface

--- backend/rentify/src/main/java/com/nocountry/rentify/model/enums/PropertyMultimediaType.java
+++ backend/rentify/src/main/java/com/nocountry/rentify/model/enums/PropertyMultimediaType.java
@@ -1,6 +1,13 @@
 package com.nocountry.rentify.model.enums;
 
-public enum PropertyMultimediaType {
-    IMAGE,
-    VIDEO
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum PropertyMultimediaType implements TranslatableEnum{
+    IMAGE("Imagen"),
+    VIDEO("Video");
+
+    private final String translatedName;
 }
