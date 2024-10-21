ea955ce658335498f752d855aed4e0d97348cc09
Angel Añez <angdavid2910@gmail.com>
2024-10-21 13:42:35 -0400

    front: feat(navbar and theme): add logo at navbar and create material ui palette theme

--- frontend/rentify/src/App.tsx
+++ frontend/rentify/src/App.tsx
@@ -6,21 +6,25 @@ import Navbar from "./components/Navbar/Navbar";
 import { AlertProvider } from "./context/Alert/AlertProvider";
 import AlertElement from "./components/Alert/AlertElement";
 import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <AuthProvider>
-      <AlertProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-          <AlertElement />
-        </Router>
-      </AlertProvider>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <AlertProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+            <AlertElement />
+          </Router>
+        </AlertProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
