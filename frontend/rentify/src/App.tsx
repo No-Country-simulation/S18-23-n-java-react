6102e3d7c0aafccfbcabe74e2d331fb4f1340807
Angel Añez <angdavid2910@gmail.com>
2024-10-23 22:41:50 -0400

    front: refactor(landing): update properties structure to match with Backend responses

--- frontend/rentify/src/App.tsx
+++ frontend/rentify/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context";
-import LandingPage from "./pages/Landing/LandigPage";
+import LandingPage from "./pages/Landing/LandingPage";
 import LoginPage from "./pages/Login/LoginPage";
 import Navbar from "./components/Navbar/Navbar";
-import { AlertProvider } from "./context/Alert/AlertProvider";
-import AlertElement from "./components/Alert/AlertElement";
-import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./theme/theme";
+import { AuthProvider, AlertProvider } from "./context";
+import AlertElement from "./components/Alert/AlertElement";
 
 function App() {
   return (
