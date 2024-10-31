b0a4bb7d65c642a974e314aec629f9bda60db9fc
kenti <016mki69@gmail.com>
2024-10-31 00:17:20 -0300

    Back: chore(V17_Add_data_multimedia_column.sql): add migration file for Postgresql

---
+++ backend/rentify/src/main/resources/db/migration/postgresql/V17__Add_data_multimedia_column.sql
@@ -0,0 +1,16 @@
+INSERT INTO properties_multimedia (type, url, property_id )
+VALUES
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331363/Cloudinary-React/gdaarakazxgmzidqbhpm.png',1),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331362/Cloudinary-React/afhdlfnjavgddsnldmpd.png',2),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331361/Cloudinary-React/hmsdqegm7eoejgndrlxz.png',3),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331361/Cloudinary-React/ycjaffo21yelcp3gyh9x.png',4),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331359/Cloudinary-React/qnmsuhurvmjfrbpkpvn9.png',5),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331359/Cloudinary-React/whxpqdorus51wo73t5xo.png',6),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331358/Cloudinary-React/qz7iulqk4jl8wdunkabs.png',7),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331356/Cloudinary-React/jt42gbgyvznp6fkpnv2o.png',8),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331356/Cloudinary-React/aky6fjdqahgjj95jii8q.png',9),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331355/Cloudinary-React/mxttmm8kdzmvxnc7y8mk.png',10),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331354/Cloudinary-React/u3gdsmghqjxh5n2mzsfk.png',11),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331354/Cloudinary-React/ozhfie898qj7dpuz8ppe.png',12),
+    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331353/Cloudinary-React/qrdsa2walruet2wbqo4k.png',13)
+;
